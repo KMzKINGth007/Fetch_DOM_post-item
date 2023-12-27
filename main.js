5797e33e037a67d3fe6326d678d5806fa383c0ba
KMzKINGth007 <151488513+KMzKINGth007@users.noreply.github.com>
2023-12-27 11:26:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,39 @@
-function makeElement(tag, attributes, text) {
-    const element = document.createElement(tag)
-    for (const key in attributes) {
-        element.setAttribute(key, attributes[key])
-    }
-    if (text) {
-        element.textContent = text
-    }
-    return element
-}
+const makeElement = (tag, attributes, text) => {
+    const element = document.createElement(tag);
+    Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value));
+    if (text) element.textContent = text;
+    return element;
+};
+
+const userList = document.querySelector('.user-list')
+const postInfo = document.querySelector('.post-info')
+
+const fetchPosts = (userId) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+        .then(response => response.json())
+        .then(posts => {
+            postInfo.innerHTML = ''
+            posts.forEach(post => {
+                const postElement = makeElement('div', { class: 'post' })
+                const titleElement = makeElement('h3', {}, post.title)
+                const bodyElement = makeElement('p', {}, post.body)
+                postElement.appendChild(titleElement)
+                postElement.appendChild(bodyElement)
+                postInfo.appendChild(postElement)
+            })
+        })
+        .catch(error => console.error('Error fetching posts:', error))
+};
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => {
-        const userList = document.querySelector('.user-list')
-
         users.forEach(user => {
-            const listItem = makeElement('li', {}, `Name:${user.name} --- Email:${user.email}`)
+            const listItem = makeElement('li', { 'data-user-id': user.id }, `${user.name} - ${user.email}`)
+            listItem.addEventListener('click', () => {
+                fetchPosts(user.id)
+            });
             userList.appendChild(listItem)
-        })
-    })
-    .catch(error => console.error('Error fetching data:', error))
-
-
-    
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(posts => {
-        const postInfo = document.querySelector('.post-info');
-
-        posts.forEach(post => {
-            const postItem = makeElement('div', { class: 'post-item' });
-            const userId = makeElement('p', {}, `User ID: ${post.userId}`);
-            const title = makeElement('p', {}, `Title: ${post.title}`);
-            const body = makeElement('p', {}, `Body: ${post.body}`);
-
-            postItem.appendChild(userId);
-            postItem.appendChild(title);
-            postItem.appendChild(body);
-
-            postInfo.appendChild(postItem);
         });
     })
-    .catch(error => console.error('Error fetching post data:', error));
+    .catch(error => console.error('Error fetching users:', error))
